1fa03d86bf99542e164012bc80f065b944e85d97
Miroslav Bajtoš <mbajtos@cz.ibm.com>
2016-08-02 15:32:51 +0200

    kvao: implement get with ttl

--- test/kv-memory.js
+++ test/kv-memory.js
@@ -2,8 +2,15 @@ var kvMemory = require('../lib/connectors/kv-memory');
 var DataSource = require('..').DataSource;
 
 describe('KeyValue-Memory connector', function() {
+  var lastDataSource;
   var dataSourceFactory = function() {
-    return new DataSource({ connector: kvMemory });
+    lastDataSource = new DataSource({ connector: kvMemory });
+    return lastDataSource;
   };
+
+  afterEach(function disconnectKVMemoryConnector() {
+    if (lastDataSource) return lastDataSource.disconnect();
+  });
+
   require('./kvao.suite')(dataSourceFactory);
 });
--- test/kvao/get-test.suite.js
+++ test/kvao/get-test.suite.js
@@ -2,6 +2,7 @@
 
 var should = require('should');
 var helpers = require('./_helpers');
+var Promise = require('bluebird');
 
 module.exports = function(dataSourceFactory, connectorCapabilities) {
   describe('get/set', function() {
@@ -10,7 +11,7 @@ module.exports = function(dataSourceFactory, connectorCapabilities) {
       CacheItem = helpers.givenCacheItem(dataSourceFactory);
     });
 
-    it('works for string values', function(done) {
+    it('works for string values - Callback API', function(done) {
       CacheItem.set('a-key', 'a-value', function(err) {
         if (err) return done(err);
         CacheItem.get('a-key', function(err, value) {
@@ -37,6 +38,27 @@ module.exports = function(dataSourceFactory, connectorCapabilities) {
       return CacheItem.set('a-key', new Buffer([1, 2, 3]))
         .then(function() { return CacheItem.get('a-key'); })
         .then(function(value) { value.should.eql(new Buffer([1, 2, 3])); });
+    });
+
+    describe('get', function() {
+      it('returns "null" when key does not exist', function() {
+        return CacheItem.get('key-does-not-exist')
+          .then(function(value) { should.equal(value, null); });
+      });
+
+      it('honours options.ttl', function() {
+        return CacheItem.set('a-key', 'a-value', { ttl: 10 })
+          .delay(20)
+          .then(function() { return CacheItem.get('a-key'); })
+          .then(function(value) { should.equal(value, null); });
+      });
+
+      it('converts numeric options arg to options.ttl', function() {
+        return CacheItem.set('a-key', 'a-value', 10)
+          .delay(20)
+          .then(function() { return CacheItem.get('a-key'); })
+          .then(function(value) { should.equal(value, null); });
+      });
     });
   });
 };
